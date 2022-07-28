b47ab90d694f79fa05a7e2b51f77d7340d4c388a
Anubhav Ghosh <88537612+Anubhav-Ghosh1@users.noreply.github.com>
2022-07-28 07:57:33 +0530

    Add files via upload

---
+++ PRIZEPOOL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int x, y;
+        cin >> x >> y;
+        cout << (10*x)+(90*y) << endl;
+    }
+	// your code goes here
+	return 0;
+}
