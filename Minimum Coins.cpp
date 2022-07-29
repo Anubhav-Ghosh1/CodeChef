0ecd3782a9c8a54fb66147f01bfdc62bc4831009
Anubhav Ghosh <88537612+Anubhav-Ghosh1@users.noreply.github.com>
2022-07-29 17:29:58 +0530

    Create Minimum Coins.cpp

---
+++ Minimum Coins.cpp
@@ -0,0 +1,16 @@
+Problem Statement ---------> https://www.codechef.com/submit/MINCOINSREQ?tab=statement
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int a;
+	    cin >> a;
+	    cout << a%10 << endl;
+	}
+	return 0;
+}
