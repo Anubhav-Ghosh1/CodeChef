63a17128b7110864ba90c10519bef874291fbed9
Anubhav Ghosh <88537612+Anubhav-Ghosh1@users.noreply.github.com>
2022-07-29 17:24:05 +0530

    Create My very 1st contest.cpp

---
+++ My very 1st contest.cpp
@@ -0,0 +1,14 @@
+Problem statement -----------> https://www.codechef.com/submit/MY1STCONTEST?tab=submissions
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+        int a , b, c;
+        cin >> a >> b >> c;
+        
+        cout << a-b << " " <<  a-b-c << endl;
+        
+    
+	return 0;
+}
